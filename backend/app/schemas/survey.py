80212ac002397c2459e3f3b52de812df8bacb70d
Volodymyr Taranin <volodymyr.taranin@gmail.com>
2026-05-15 20:01:38 +0300

    feat(backend): add survey schemas

---
+++ backend/app/schemas/survey.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+
+class SurveyBase(BaseModel):
+    genres_prefer: list[int]
+    genres_avoid: list[int]
+    animes_prefer: list[int]
+    characters_prefer: list[int]
+
+
+class SurveyCreate(SurveyBase):
+    pass
+
+
+class SurveyRead(SurveyBase):
+    id: int
+    user_id: int
+    created_at: datetime
+    updated_at: datetime
+
+
+SurveyUpdate = SurveyCreate
